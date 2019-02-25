a46ccc4af655d04aa2535a4e0ac5caecab7bcceb
Dimitar Shterionov <dimitarsh1@samurai-gtx1080ti.computing.dcu.ie>
2019-02-25 17:35:28 +0000

    Scripts for training, testing, evaluation - Pylint and autopep8 on score.py

--- scripts/score.py
+++ scripts/score.py
@@ -7,6 +7,7 @@ import codecs
 import os
 import sys
 
+
 def main():
     ''' main function '''
     # read argument - file with data
@@ -19,8 +20,8 @@ def main():
     # initialize a vocabulary as a set (only one occurrence of an element)
     data_vocabulary = {}
     if os.path.exists(args.input):
-        with codecs.open(args.input, 'r', 'utf8') as fh:
-            for line in fh:
+        with codecs.open(args.input, 'r', 'utf8') as ifh:
+            for line in ifh:
                 # do the union to add more elements to the vocabulary
                 for token in line.split():
                     if token not in data_vocabulary:
@@ -30,7 +31,7 @@ def main():
 
         # print the vocabulary to the file.
         with codecs.open(args.outfile, 'w', 'utf8') as ofh:
-            ofh.write("\n".join([x + '\t' + str(a[x]) for x in sorted(a, key=a.get, reverse=True)]) + '\n')
+            ofh.write("\n".join([x + '\t' + str(data_vocabulary[x]) for x in sorted(data_vocabulary, key=data_vocabulary.get, reverse=True)]) + '\n')
     else:
         # if file doesn't exist exit and print a message to stderr`
         print("ERROR: Path not found: ", args.input, file=sys.stderr)
