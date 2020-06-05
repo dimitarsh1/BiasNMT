919954dcd55ddcd88d2fcfd10018115018798a44
Dimitar Shterionov <dimitarsh1@gmail.com>
2020-06-05 22:01:35 +0100

    Extra results of LD

--- scripts/diversity/ld_pairwise_script.sh
+++ scripts/diversity/ld_pairwise_script.sh
@@ -3,24 +3,24 @@ A=$1 #this is the language pair
 
 mkdir results_ld -p
 
-#	train-${A}-ORGNL.tok \
 
 ld_pairwise.py -f \
+    train-${A}-ORGNL.tok \
 	train-${A}-RBMT.unk.tok \
 	train-${A}-RBMT.unk.tok.onl \
-	train-${A}-RBMT.unk.tok.noerr > results_ld/ld_${A}_RMBT.out #\
-#	train-${A}-SMT.out.tok \
-#	train-${A}-SMT-UNKN.out.tok \
-#	train-${A}-LSTM-BPE.out.tok \
-#	train-${A}-LSTM-BPE.out.tok.nobpe \
-#	train-${A}-TRANS-BPE.out.tok \
-#	train-${A}-TRANS-BPE.out.tok.nobpe > results/ld_$A.out;
+	train-${A}-RBMT.unk.tok.noerr \
+	train-${A}-SMT.out.tok \
+	train-${A}-SMT-UNKN.out.tok \
+	train-${A}-LSTM-BPE.out.tok \
+	train-${A}-LSTM-BPE.out.tok.nobpe \
+	train-${A}-TRANS-BPE.out.tok \
+	train-${A}-TRANS-BPE.out.tok.nobpe > results_ld/ld_$A.out;
 
-#ld_pairwise.py -f \
-#    train-${A}-SMT.back.out.tok \
-#    train-${A}-SMT-UNKN.back.out.tok \
-#    train-${A}-LSTM-BPE.back.out.tok \
-#    train-${A}-LSTM-BPE.back.out.tok.nobpe \
-#    train-${A}-TRANS-BPE.back.out.tok \
-#    train-${A}-TRANS-BPE.back.out.tok.nobpe > results/ld_${A}_back.out;
+ld_pairwise.py -f \
+    train-${A}-SMT.back.out.tok \
+    train-${A}-SMT-NODUP-UNKN.back.out.tok \
+    train-${A}-LSTM-BPE.back.out.tok \
+    train-${A}-LSTM-BPE.back.out.tok.nobpe \
+    train-${A}-TRANS-BPE.back.out.tok \
+    train-${A}-TRANS-BPE.back.out.tok.nobpe > results_ld/ld_${A}_back.out;
 
--- scripts/diversity/lexical_profile_script.sh
+++ scripts/diversity/lexical_profile_script.sh
@@ -4,16 +4,16 @@ echo $A
 
 mkdir results_lfp -p
 
-#lexical_profile.py -f train-${A}-ORGNL.tok > results_lfp/lfp_train-${A}_ORGNL.out &
+lexical_profile.py -f train-${A}-ORGNL.tok > results_lfp/lfp_train-${A}_ORGNL.out &
 lexical_profile.py -f train-${A}-RBMT.unk.tok.onl > results_lfp/lfp_train-${A}_RBMT_UNKN.out &
 lexical_profile.py -f train-${A}-RBMT.unk.tok.noerr > results_lfp/lfp_train-${A}_RBMT_NOERR.out &
-#lexical_profile.py -f train-${A}-SMT-UNKN.out.tok > results_lfp/lfp_train-${A}_SMT_UNKN.out &
-#lexical_profile.py -f train-${A}-LSTM-BPE.out.tok.nobpe > results_lfp/lfp_train-${A}_LSTM_NOBPE.out &
-#lexical_profile.py -f train-${A}-TRANS-BPE.out.tok.nobpe > results_lfp/lfp_train-${A}_TRANS_NOBPE.out &
+lexical_profile.py -f train-${A}-SMT-UNKN.out.tok > results_lfp/lfp_train-${A}_SMT_UNKN.out &
+lexical_profile.py -f train-${A}-LSTM-BPE.out.tok.nobpe > results_lfp/lfp_train-${A}_LSTM_NOBPE.out &
+lexical_profile.py -f train-${A}-TRANS-BPE.out.tok.nobpe > results_lfp/lfp_train-${A}_TRANS_NOBPE.out &
 
 # backtranslated systems:
-#lexical_profile.py -f train-${A}-SMT-NODUP-UNKN.back.out.tok > results_lfp/lfp_back_train-${A}_SMT_UNKN.out &
-#lexical_profile.py -f train-${A}-LSTM-BPE.back.out.tok.nobpe > results_lfp/lfp_back_train-${A}_LSTM_NOBPE.out &
-#lexical_profile.py -f train-${A}-TRANS-BPE.back.out.tok.nobpe > results_lfp/lfp_back_train-${A}_TRANS_NOBPE.out &
+lexical_profile.py -f train-${A}-SMT-NODUP-UNKN.back.out.tok > results_lfp/lfp_back_train-${A}_SMT_UNKN.out &
+lexical_profile.py -f train-${A}-LSTM-BPE.back.out.tok.nobpe > results_lfp/lfp_back_train-${A}_LSTM_NOBPE.out &
+lexical_profile.py -f train-${A}-TRANS-BPE.back.out.tok.nobpe > results_lfp/lfp_back_train-${A}_TRANS_NOBPE.out &
 wait
 
